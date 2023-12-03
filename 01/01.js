741f4aa88cad65b602bb282521a2e8da1d4f932d
giacomocerquone <cerquone96@hotmail.it>
2023-12-04 00:58:57 +0100

    complete part2

--- 01/01.js
+++ 01/01.js
@@ -1,15 +1,60 @@
 const fs = require("fs");
 const value = fs.readFileSync("./input.txt", "utf8");
 
-const total = value.split("\n").reduce((acc, line) => {
-  const first = line.search(/\d/);
-  const reversed = line.split("").reverse().join("");
-  const second = reversed.search(/\d/);
+const PART_TWO = true;
 
-  if (first !== -1) {
-    return (
-      acc + parseInt(`${line.charAt(first)}${reversed.charAt(second)}`, 10)
+const numsMap = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+const parseLine = (rawLine) => {
+  const firstReplaced = rawLine.replace(
+    /(one|two|three|four|five|six|seven|eight|nine)/,
+    (match, p1) => numsMap[p1.trim()]
+  );
+
+  const occurrences = Object.keys(numsMap).map((number) => {
+    return { lastIndexOf: firstReplaced.lastIndexOf(number), number };
+  });
+  const lastOccurrence = occurrences.reduce(function (prev, current) {
+    return prev.lastIndexOf > current.lastIndexOf ? prev : current;
+  }, {});
+
+  if (lastOccurrence?.lastIndexOf !== -1) {
+    const lastReplaced =
+      firstReplaced.slice(0, lastOccurrence.lastIndexOf) +
+      numsMap[lastOccurrence.number] +
+      firstReplaced.slice(
+        lastOccurrence.lastIndexOf + lastOccurrence.number.length
+      );
+
+    // console.log(firstReplaced, lastReplaced);
+
+    return lastReplaced;
+  }
+
+  return firstReplaced;
+};
+
+const total = value.split("\n").reduce((acc, rawLine) => {
+  const line = PART_TWO ? parseLine(rawLine) : rawLine;
+  const matches = line.match(/(\d)/g);
+
+  if (matches.length > 0) {
+    console.log(
+      line,
+      parseInt(`${matches[0]}${matches[matches.length - 1]}`, 10)
     );
+
+    return acc + parseInt(`${matches[0]}${matches[matches.length - 1]}`, 10);
   }
 
   return acc;
